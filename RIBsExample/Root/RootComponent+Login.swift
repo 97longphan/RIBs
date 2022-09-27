84f8e89dce5b0bd5efe8a76b344db24c91c4a351
phlong <97longphan@gmail.com>
2022-09-28 00:47:41 +0700

    update

---
+++ RIBsExample/Root/RootComponent+Login.swift
@@ -0,0 +1,16 @@
+//
+//  RootComponent+Login.swift
+//  RIBsExample
+//
+//  Created by Phan Long on 28/09/2022.
+//
+
+import RIBs
+
+protocol RootDependencyLogin: Dependency {
+    
+}
+
+extension RootComponent: LoginDependency {
+    
+}
