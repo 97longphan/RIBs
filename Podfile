1585cd87ed35023a1c55875a0c87d52db17a3da0
long.phan <long.phan@onemount.com>
2022-09-27 15:40:41 +0700

    update new

---
+++ Podfile
@@ -0,0 +1,11 @@
+# Uncomment the next line to define a global platform for your project
+# platform :ios, '9.0'
+
+target 'RIBsExample' do
+  # Comment the next line if you don't want to use dynamic frameworks
+  use_frameworks!
+
+  # Pods for RIBsExample
+  pod 'RIBs', '~> 0.9'
+  pod 'RxCocoa'
+end
---
+++ RIBsExample/AppDelegate.swift
@@ -0,0 +1,24 @@
+//
+//  AppDelegate.swift
+//  RIBsExample
+//
+//  Created by LONGPHAN on 27/09/2022.
+//
+
+import UIKit
+import RIBs
+
+@main
+class AppDelegate: UIResponder, UIApplicationDelegate {
+
+
+
+    func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
+        // Override point for customization after application launch.
+        return true
+    }
+
+
+
+}
+
